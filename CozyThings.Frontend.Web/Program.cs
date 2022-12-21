9eb2b644c580b4c16828ccc056ba8dcecde88cb2
Andrii Korshenko <andriikorshenko@gmail.com>
2022-12-21 21:28:14 +0100

    Implemented ProductService

--- CozyThings.Frontend.Web/Program.cs
+++ CozyThings.Frontend.Web/Program.cs
@@ -1,15 +1,20 @@
+using CozyThings.Frontend.Web;
+using CozyThings.Frontend.Web.Services;
+using CozyThings.Frontend.Web.Services.Imp;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+StaticDetails.ProductApiBase = builder.Configuration["ServiceUrls:ProductApi"];
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
---
+++ CozyThings.Frontend.Web/Services/Imp/ProductService.cs
@@ -0,0 +1,63 @@
+﻿using CozyThings.Frontend.Web.Models;
+using CozyThings.Frontend.Web.Models.Product;
+
+namespace CozyThings.Frontend.Web.Services.Imp
+{
+    public class ProductService : BaseService, IProductService
+    {
+        private readonly IConfiguration configuration;
+
+        public ProductService(IHttpClientFactory httpClientFactory, IConfiguration configuration) 
+            : base(httpClientFactory)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<T> GetAllProductsAsync<T>()
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = ApiType.GET,
+                Url = StaticDetails.ProductApiBase + "/api/products"
+            });
+        }
+
+        public async Task<T> GetProductByIdAsync<T>(int id)
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = ApiType.GET,
+                Url = StaticDetails.ProductApiBase + "/api/products/" + id
+            });
+        }
+
+        public async Task<T> CreateProductAsync<T>(ProductCreateDto dto)
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = ApiType.POST,
+                Data = dto,
+                Url = StaticDetails.ProductApiBase + "/api/products"
+            });
+        }
+
+        public async Task<T> UpdateProductAsync<T>(ProductUpdateDto dto)
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = ApiType.PUT,
+                Data = dto,
+                Url = StaticDetails.ProductApiBase + "/api/products"
+            });
+        }
+
+        public async Task<T> DeleteProductAsync<T>(int id)
+        {
+            return await this.SendAsync<T>(new ApiRequest
+            {
+                ApiType = ApiType.DELETE,
+                Url = StaticDetails.ProductApiBase + "/api/products/" + id
+            });
+        }  
+    }
+}
