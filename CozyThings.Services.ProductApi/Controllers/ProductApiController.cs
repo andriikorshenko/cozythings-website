4f16f9e0c8b8c306ac85c9f5495b85b056448937
Andrii Korshenko <andriikorshenko@gmail.com>
2022-12-21 19:54:54 +0100

    Implemented CRUD-operations for ProductApiController

---
+++ CozyThings.Services.ProductApi/Controllers/ProductApiController.cs
@@ -0,0 +1,100 @@
+﻿using CozyThings.Services.ProductApi.Models;
+using CozyThings.Services.ProductApi.Models.Product;
+using CozyThings.Services.ProductApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CozyThings.Services.ProductApi.Controllers
+{
+    [Route("api/products")]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+        private readonly ResponseDto response;
+
+        public ProductApiController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+            this.response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<ResponseDto> Get()
+        {
+            try
+            {
+                var products = await productRepository.GetProducts();
+                response.Result = products;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return response;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ResponseDto> Get(int id)
+        {
+            try
+            {
+                var products = await productRepository.GetProductById(id);
+                response.Result = products;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return response;
+        }
+
+        [HttpPost]
+        public async Task<ResponseDto> Create([FromBody] ProductCreateDto dto)
+        {
+            try
+            {
+                var product = await productRepository.CreateProduct(dto);
+                response.Result = product;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return response;
+        }
+
+        [HttpPut]
+        public async Task<ResponseDto> Update([FromBody] ProductUpdateDto dto)
+        {
+            try
+            {
+                var product = await productRepository.UpdateProduct(dto);
+                response.Result = product;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return response;
+        }
+
+        [HttpDelete]
+        public async Task<ResponseDto> Delete(int id)
+        {
+            try
+            {
+                var result = await productRepository.DeleteProduct(id);
+                response.Result = result;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return response;
+        }
+    }
+}
--- CozyThings.Services.ProductApi/Models/ResponseDto.cs
+++ CozyThings.Services.ProductApi/Models/ResponseDto.cs
@@ -2,5 +2,9 @@
 {
     public class ResponseDto
     {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = string.Empty;
+        public IReadOnlyList<string> ErrorMessages { get; set; } = Array.Empty<string>();
     }
 }
