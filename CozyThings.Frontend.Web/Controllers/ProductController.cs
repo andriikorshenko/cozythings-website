2a9f8aa6ad157dd8f47c8fdd9a54a290dad1c174
Andrii Korshenko <andriikorshenko@gmail.com>
2022-12-22 00:10:27 +0100

    Added Index View and Action of ProductController

---
+++ CozyThings.Frontend.Web/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using CozyThings.Frontend.Web.Models;
+using CozyThings.Frontend.Web.Models.Product;
+using CozyThings.Frontend.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CozyThings.Frontend.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<ProductDto> products = new();
+
+            var response = await productService.GetAllProductsAsync<ResponseDto>();
+            if (response != null & response.IsSuccess)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(products);
+        }
+    }
+}
