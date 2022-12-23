d799f924430d768a9eae0ca1254d884fe99dae35
Andrii Korshenko <andriikorshenko@gmail.com>
2022-12-23 23:01:42 +0100

    Configured Identity Server and first Migration

--- CozyThings.Services.Identity/Program.cs
+++ CozyThings.Services.Identity/Program.cs
@@ -1,15 +1,38 @@
+using CozyThings.Services.Identity;
+using CozyThings.Services.Identity.Data;
+using CozyThings.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddDbContext<AppDbContext>(options
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+.AddInMemoryIdentityResources(StaticDetails.IdentityResources)
+.AddInMemoryApiScopes(StaticDetails.ApiScopes)
+.AddInMemoryClients(StaticDetails.CLients)
+.AddAspNetIdentity<ApplicationUser>()
+.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,7 +40,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
---
+++ CozyThings.Services.Identity/StaticDetails.cs
@@ -0,0 +1,55 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace CozyThings.Services.Identity
+{
+    public static class StaticDetails
+    {
+        public const string ADMIN = "Admin";
+        public const string CUSTOMER = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope> 
+            { 
+                new ApiScope("cozythings", "cozythings server"), 
+                new ApiScope(name: "read", displayName: "Read your date"),
+                new ApiScope(name: "write", displayName: "Write your date"),
+                new ApiScope(name: "delete", displayName: "Delete your date")
+            };
+
+        public static IEnumerable<Client> CLients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "read", "write", "profile" }
+                },
+                new Client
+                {
+                    ClientId = "cozythings",
+                    ClientSecrets = { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:44379/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:44379/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "cozythings"
+                    }
+                }
+            };
+    }
+}
