262d4e5b833b54e62d0ba6a9ea5bcc48fac5ec70
Andrii Korshenko <andriikorshenko@gmail.com>
2022-12-23 22:11:08 +0100

    Created IdentityDbContext for Identity Server

---
+++ CozyThings.Services.Identity/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CozyThings.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozyThings.Services.Identity.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+    }
+}
---
+++ CozyThings.Services.Identity/Data/MigrationContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozyThings.Services.Identity.Data
+{
+    public class MigrationContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString, opts
+                => opts.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds));
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
