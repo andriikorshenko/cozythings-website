bb6d473833080ca2e648c095d17dbf5ff2b30d14
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-01-01 18:24:22 +0100

    Created Service for CouponApi

---
+++ CozyThings.Services.CouponApi/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace CozyThings.Services.CouponApi
+{
+    public class MappingConfig 
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                /*config.CreateMap<ProductDto, Product>().ReverseMap();
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                config.CreateMap<Cart, CartDto>().ReverseMap();*/
+            });
+
+            return mappingConfig;
+        }
+    }
+}
