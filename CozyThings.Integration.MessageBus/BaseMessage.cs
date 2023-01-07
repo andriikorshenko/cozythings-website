4eeb2ae48278c2d64e735a64abd880a14e650108
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-01-07 23:11:52 +0100

    Scaffolding new project for OrderApi + NuGet packages

--- CozyThings.Integration.MessageBus/BaseMessage.cs
+++ CozyThings.Integration.MessageBus/BaseMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CozyThings.Integration.MessageBus
+﻿namespace CozyThings.Integration.MessageBus
 {
     public class BaseMessage
     {
