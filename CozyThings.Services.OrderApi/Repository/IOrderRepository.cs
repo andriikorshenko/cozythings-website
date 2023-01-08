7d025dc5ad19b7661f633fa1cc20a20c07b49824
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-01-08 19:07:32 +0100

    Implemented OrderRepository

---
+++ CozyThings.Services.OrderApi/Repository/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using CozyThings.Services.OrderApi.Data.Entities;
+
+namespace CozyThings.Services.OrderApi.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader orderHeader);
+
+        Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+    }
+}
---
+++ CozyThings.Services.OrderApi/Repository/Imp/OrderRepository.cs
@@ -0,0 +1,35 @@
+﻿using CozyThings.Services.OrderApi.Data;
+using CozyThings.Services.OrderApi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozyThings.Services.OrderApi.Repository.Imp
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DbContextOptions<AppDbContext> dbContext;
+
+        public OrderRepository(DbContextOptions<AppDbContext> dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+            await using var db = new AppDbContext(dbContext);
+            db.OrderHeaders.Add(orderHeader);
+            await db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        {
+            await using var db = new AppDbContext(dbContext);
+            var orderHeaderFromDb = await db.OrderHeaders.FirstOrDefaultAsync(x => x.Id == orderHeaderId);
+            if (orderHeaderFromDb != null)
+            {
+                orderHeaderFromDb.PaymentStatus = paid;
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
