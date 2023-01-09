430b1111e7948ff0a91cb168a28883fd392dad0c
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-01-09 21:47:42 +0100

    Added Models for Azure ServiceBus messeging

---
+++ CozyThings.Services.OrderApi/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Messaging.ServiceBus;
+using CozyThings.Services.OrderApi.Models;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace CozyThings.Services.OrderApi.Messaging
+{
+    public class AzureServiceBusConsumer
+    {
+        private async Task OnCheckoutMessageReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
+        }
+    }
+}
